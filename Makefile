1fea06ed297c2087f4ad8c96cd9ca63a6910e0e2
Guilherme Soares <guilherme.soares@iovlabs.org>
2024-05-21 19:09:56 +0200

    feat: 1 - Transport layer

---
+++ Makefile
@@ -0,0 +1,9 @@
+build:
+	go mod tidy
+	go build -o ./bin/go-blockchain
+
+run: build
+	./bin/go-blockchain
+
+test:
+	go test -v ./...
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gsoares85/go-blockchain/network"
+	"time"
+)
+
+func main() {
+	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		for {
+			trRemote.SendMessage(trLocal.Addr(), []byte("Hello World"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trLocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
+}
