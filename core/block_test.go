766ec75437490e704bbc45f5274acc3b43a31d7c
Guilherme Soares <guilherme.soares@iovlabs.org>
2024-06-06 13:04:07 +0200

    feat: EP05

--- core/block_test.go
+++ core/block_test.go
@@ -22,6 +22,15 @@ func randomBlock(height uint32) *Block {
 	return NewBlock(header, []Transaction{tx})
 }
 
+func randomBlockWithSignature(t *testing.T, height uint32) *Block {
+	privKey := crypto.GeneratePrivateKey()
+	b := randomBlock(height)
+	b.Sign(privKey)
+	assert.Nil(t, b.Sign(privKey))
+
+	return b
+}
+
 func TestSignBlock(t *testing.T) {
 	privaKey := crypto.GeneratePrivateKey()
 	b := randomBlock(0)
