aed68f0de3f1ac66780b02b0cd213b777002ea1a
Guilherme Soares <guilherme.soares@iovlabs.org>
2024-06-07 17:46:39 +0200

    feat: EP06

--- core/blockchain_test.go
+++ core/blockchain_test.go
@@ -1,16 +1,10 @@
 package core
 
 import (
+	"github.com/gsoares85/go-blockchain/types"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
-
-func newBlockchainWithGenesis(t *testing.T) *Blockchain {
-	bc, err := NewBlockchain(randomBlock(0))
-	assert.Nil(t, err)
-
-	return bc
-}
 
 func TestBlockchain(t *testing.T) {
 	bc := newBlockchainWithGenesis(t)
@@ -21,6 +15,8 @@ func TestBlockchain(t *testing.T) {
 func TestHashBlock(t *testing.T) {
 	bc := newBlockchainWithGenesis(t)
 	assert.True(t, bc.HasBlock(0))
+	assert.False(t, bc.HasBlock(1))
+	assert.False(t, bc.HasBlock(100))
 }
 
 func TestAddBlock(t *testing.T) {
@@ -28,11 +24,45 @@ func TestAddBlock(t *testing.T) {
 
 	lenBlocks := 1000
 	for i := 0; i < lenBlocks; i++ {
-		block := randomBlockWithSignature(t, uint32(i)+1)
+		block := randomBlockWithSignature(t, uint32(i)+1, getPrevBlockHash(t, bc, uint32(i+1)))
 		assert.Nil(t, bc.AddBlock(block))
 	}
 
 	assert.Equal(t, bc.Height(), uint32(lenBlocks))
 	assert.Equal(t, len(bc.headers), lenBlocks+1)
-	assert.NotNil(t, bc.AddBlock(randomBlock(89)))
+	assert.NotNil(t, bc.AddBlock(randomBlock(89, types.Hash{})))
+}
+
+func TestAddBlockToHigh(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+
+	assert.Nil(t, bc.AddBlock(randomBlockWithSignature(t, 1, getPrevBlockHash(t, bc, uint32(1)))))
+	assert.NotNil(t, bc.AddBlock(randomBlockWithSignature(t, 3, types.Hash{})))
+}
+
+func TestGetHeader(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+
+	lenBlocks := 1000
+	for i := 0; i < lenBlocks; i++ {
+		block := randomBlockWithSignature(t, uint32(i)+1, getPrevBlockHash(t, bc, uint32(i+1)))
+		assert.Nil(t, bc.AddBlock(block))
+		header, err := bc.GetHeader(block.Height)
+		assert.Nil(t, err)
+		assert.Equal(t, header, block.Header)
+	}
+}
+
+func newBlockchainWithGenesis(t *testing.T) *Blockchain {
+	bc, err := NewBlockchain(randomBlock(0, types.Hash{}))
+	assert.Nil(t, err)
+
+	return bc
+}
+
+func getPrevBlockHash(t *testing.T, bc *Blockchain, height uint32) types.Hash {
+	prevHeader, err := bc.GetHeader(height - 1)
+	assert.Nil(t, err)
+
+	return BlockHasher{}.Hash(prevHeader)
 }
--- core/transaction_test.go
+++ core/transaction_test.go
@@ -25,11 +25,22 @@ func TestVerifyTransaction(t *testing.T) {
 	}
 	signErr := tx.Sign(privKey)
 	assert.Nil(t, signErr)
-	assert.Equal(t, privKey.PublicKey(), tx.PublicKey)
+	assert.Equal(t, privKey.PublicKey(), tx.From)
 	assert.Nil(t, tx.Verify())
 
 	otherPrivKey := crypto.GeneratePrivateKey()
-	tx.PublicKey = otherPrivKey.PublicKey()
+	tx.From = otherPrivKey.PublicKey()
 
 	assert.NotNil(t, tx.Verify())
+}
+
+func randomTxWithSignature(t *testing.T) *Transaction {
+	privKey := crypto.GeneratePrivateKey()
+	tx := &Transaction{
+		Data: []byte("foo"),
+	}
+
+	assert.Nil(t, tx.Sign(privKey))
+
+	return tx
 }
